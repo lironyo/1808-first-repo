40388de4fbd7f50cbbe24caf3048b9f46b4a9d5d
Liron Yona <121067425+lironyo@users.noreply.github.com>
2025-11-03 20:54:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-def calculate(x, y):
-    return x + y
-
-def run():
-    a = 10
-    b = 5
-    result = calculate(a, b)
-    
-    print(result)
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello from GitHub Actions + Docker!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
